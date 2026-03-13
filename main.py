e05a1903af730802daacb62e9c882a297444d9db
Artsiom Pipchanka <artpip@st.amu.edu.pl>
2026-03-13 08:52:33 +0100

    feat: added tests

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 def hello():
-    return {"message": "Hello World"}
+    return {"message": "czesc swiat"}
+
+
+@app.get("/add/{a}/{b}")
+def add(a: int, b: int):
+    return {"result": a + b}
