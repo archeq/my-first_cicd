792676e6d89e2422b66ea61f60e440c1308a8091
Artsiom Pipchanka <artpip@st.amu.edu.pl>
2026-03-13 09:01:11 +0100

    feat: update test response message in test_hello

--- test_main.py
+++ test_main.py
@@ -7,7 +7,7 @@ client = TestClient(app)
 def test_hello():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "czesc swiat"}
+    assert response.json() == {"message": "czesc swiat nowy test"}
 
 
 def test_add_positive_numbers():
